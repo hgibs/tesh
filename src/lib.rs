73e46350bba513ffd134ea9aad983b62c79a110f
Holland Gibson <holland@hollandgibson.com>
2023-08-09 23:46:31 -0400

    more restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,121 @@
+// use std::str::Matches;
+
 use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Debug, Copy, Clone)]
-pub enum TranslationTarget {
+pub enum SelectionType {
+    Segment,
+    Grapheme,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum TranspositionMethod {
     UpperCase,
-    SnakeCase,
+    // SnakeCase,
 }
 
-pub fn select_target(query: String) -> TranslationTarget {
-    match query.as_str() {
-        "upper" => TranslationTarget::UpperCase,
-        "snake" => TranslationTarget::SnakeCase,
-        _ => TranslationTarget::UpperCase,
-    }
+#[derive(Debug, Copy, Clone)]
+pub struct TranslationTarget {
+    operate_on: SelectionType,
+    transpose_to: TranspositionMethod,
 }
 
-#[must_use]
-pub fn translate(source: String, target_type: TranslationTarget) -> String {
-    let graphemes = source.graphemes(true);
-    let mut result = String::new();
+// available targets
+pub const UPPERCASE: TranslationTarget = TranslationTarget {
+    operate_on: SelectionType::Grapheme,
+    transpose_to: TranspositionMethod::UpperCase,
+};
+pub const TITLECASE: TranslationTarget = TranslationTarget {
+    operate_on: SelectionType::Segment,
+    transpose_to: TranspositionMethod::UpperCase,
+};
 
-    for g in graphemes {
-        result.push_str(transpose_grapheme(g, &target_type).as_str());
-    }
-
-    result
-}
-
-fn transpose_grapheme(g: &str, target_type: &TranslationTarget) -> String {
-    let converted_g: String = if g.len() == 1 {
-        // Basic Latin
-        let byte_list = g.as_bytes();
-        let first_byte: u8 = byte_list[0];
-        print!("{}", first_byte as char);
-        let new_value: char = match target_type {
-            TranslationTarget::UpperCase => transpose_byte_to_uppercase(first_byte),
-            TranslationTarget::SnakeCase => transpose_byte_to_uppercase(first_byte),
+impl TranslationTarget {
+    pub fn translate(&self, source: String) -> String {
+        let segments = match &self.operate_on {
+            SelectionType::Segment => panic!(),
+            // SelectionType::Segment => segments(source.as_str()),
+            SelectionType::Grapheme => source.graphemes(true),
         };
-        new_value.to_string()
-    } else {
-        print!("{}", g);
-        String::from(g)
-    };
 
-    println!(" -> {}", converted_g);
-
-    converted_g
-}
-
-fn transpose_byte_to_uppercase(b: u8) -> char {
-    if 97 <= b && b <= 122 {
-        // Latin Uppercase
-        b.saturating_sub(32) as char
-    } else {
-        b as char
+        return String::from("TO DO");
     }
 }
+
+// fn segments<'a>(query: &str) -> impl Iterator<Item = &'a str> {
+//     String::from(query).graphemes(true).collect::<&str>()
+// }
+
+// #[must_use]
+// pub fn translate(source: String, target_type: TranslationTarget) -> String {
+//     let graphemes = source.graphemes(true);
+//     let mut result = String::new();
+
+//     for g in graphemes {
+//         result.push_str(transpose_grapheme(g, &target_type).as_str());
+//     }
+
+//     result
+// }
+
+// fn select_first_byte(graph: &str) -> char {
+//     let byte_list = graph.as_bytes();
+//     byte_list[0] as char
+// }
+
+// fn transpose_grapheme(g: &str, target_type: &TranslationTarget) -> String {
+//     print!("{}", g);
+//     let converted_g: String = if g.len() == 1 {
+//         // Basic Latin
+//         let new_value: char = match target_type {
+//             TranslationTarget::UpperCase => transpose_grapheme_to_uppercase(g),
+//             // TranslationTarget::SnakeCase => transpose_grapheme_to_uppercase(g),
+//             // TranslationTarget::UpperCase =>
+//         };
+//         new_value.to_string()
+//     } else {
+//         String::from(g)
+//     };
+
+//     println!(" -> {}", converted_g);
+
+//     converted_g
+// }
+
+// fn transpose_grapheme_to_uppercase(g: &str) -> char {
+//     let b = select_first_byte(g) as u8;
+
+//     if 97 <= b && b <= 122 {
+//         // Latin Uppercase
+//         b.saturating_sub(32) as char
+//     } else {
+//         b as char
+//     }
+// }
 
 #[cfg(test)]
 #[allow(unused_imports)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_translate_uppercase() {
-        static TGT: TranslationTarget = TranslationTarget::UpperCase;
-        assert_eq!(
-            translate(String::from("abcdev"), TGT),
-            String::from("ABCDEV")
-        );
-        assert_eq!(
-            translate(String::from("zyx🤣w123"), TGT),
-            String::from("ZYX🤣W123")
-        );
-    }
+    // #[test]
+    // fn test_uppercase() {
+    //     static TGT = UPPERCASE;
+    //     assert_eq!(
+
+    //     )
+    // }
+
+    // #[test]
+    // fn test_translate_uppercase() {
+    //     static TGT: TranslationTarget = TranslationTarget::UpperCase;
+    //     assert_eq!(
+    //         translate(String::from("abcdev"), TGT),
+    //         String::from("ABCDEV")
+    //     );
+    //     assert_eq!(
+    //         translate(String::from("zyx🤣w123"), TGT),
+    //         String::from("ZYX🤣W123")
+    //     );
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use clap::{command, Arg};
-use teshr::{select_target, translate, TranslationTarget};
+use teshr;
 
 fn main() {
     let matches = command!().arg(Arg::new("to_format")).get_matches();
     let to_format = matches.get_one::<String>("to_format");
 
     let target_enum = match to_format {
-        None => TranslationTarget::UpperCase,
+        None => panic!("handle this"),
         Some(t) => select_target(t.clone()),
     };
     println!("got: {:?}", target_enum);
-    translate(String::from("te😀st"), target_enum);
+    target_enum.translate(String::from("te😀st"));
+    target_enum.translate(String::from("snake_case_test"));
+}
+
+fn select_target(query: String) -> teshr::TranslationTarget {
+    match query.as_str() {
+        "title" => teshr::TITLECASE,
+        "upper" => teshr::UPPERCASE,
+        // "snake" => TranslationTarget::SnakeCase,
+        _ => panic!("unknown operation: {}", query),
+    }
 }

301308b48ea2eca31fa9bdaef4fd11faa648a71c
Holland Gibson <holland@hollandgibson.com>
2023-08-09 22:15:43 -0400

    better structure, with working latin uppercase

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,76 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+#[derive(Debug, Copy, Clone)]
 pub enum TranslationTarget {
     UpperCase,
+    SnakeCase,
 }
 
-#[must_use] pub fn translate(source: String, target_type: TranslationTarget) -> String {
-    match target_type {
-        TranslationTarget::UpperCase => translate_uppercase(source),
+pub fn select_target(query: String) -> TranslationTarget {
+    match query.as_str() {
+        "upper" => TranslationTarget::UpperCase,
+        "snake" => TranslationTarget::SnakeCase,
+        _ => TranslationTarget::UpperCase,
     }
 }
 
-fn translate_uppercase(source: String) -> String {
-    let cbytes = source.chars().into_iter().map(|x| x as u8);
-    let tgt_bytes: Vec<u8> = cbytes.map(|x| x - 32).collect();
-    let tgt_chars: Vec<char> = tgt_bytes.into_iter().map(|x| x as char).collect();
+#[must_use]
+pub fn translate(source: String, target_type: TranslationTarget) -> String {
+    let graphemes = source.graphemes(true);
+    let mut result = String::new();
 
-    tgt_chars.into_iter().collect()
+    for g in graphemes {
+        result.push_str(transpose_grapheme(g, &target_type).as_str());
+    }
+
+    result
+}
+
+fn transpose_grapheme(g: &str, target_type: &TranslationTarget) -> String {
+    let converted_g: String = if g.len() == 1 {
+        // Basic Latin
+        let byte_list = g.as_bytes();
+        let first_byte: u8 = byte_list[0];
+        print!("{}", first_byte as char);
+        let new_value: char = match target_type {
+            TranslationTarget::UpperCase => transpose_byte_to_uppercase(first_byte),
+            TranslationTarget::SnakeCase => transpose_byte_to_uppercase(first_byte),
+        };
+        new_value.to_string()
+    } else {
+        print!("{}", g);
+        String::from(g)
+    };
+
+    println!(" -> {}", converted_g);
+
+    converted_g
+}
+
+fn transpose_byte_to_uppercase(b: u8) -> char {
+    if 97 <= b && b <= 122 {
+        // Latin Uppercase
+        b.saturating_sub(32) as char
+    } else {
+        b as char
+    }
 }
 
 #[cfg(test)]
+#[allow(unused_imports)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_translate_uppercase() {
+        static TGT: TranslationTarget = TranslationTarget::UpperCase;
         assert_eq!(
-            translate_uppercase(String::from("abcdev")),
+            translate(String::from("abcdev"), TGT),
             String::from("ABCDEV")
-        )
+        );
+        assert_eq!(
+            translate(String::from("zyx🤣w123"), TGT),
+            String::from("ZYX🤣W123")
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{command, Arg};
+use teshr::{select_target, translate, TranslationTarget};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!().arg(Arg::new("to_format")).get_matches();
+    let to_format = matches.get_one::<String>("to_format");
+
+    let target_enum = match to_format {
+        None => TranslationTarget::UpperCase,
+        Some(t) => select_target(t.clone()),
+    };
+    println!("got: {:?}", target_enum);
+    translate(String::from("te😀st"), target_enum);
 }
